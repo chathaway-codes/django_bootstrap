c45ec45bc618739fd05e4ea72b07c8637ea7bc23
Charles H <chathaway@logrit.com>
2013-03-03 02:16:05 -0500

    Things and such!

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from distutils.core import setup
 
 setup(
-    name='django-boostrap',
+    name='django_boostrap',
     version='2.3.1',
     author=u'Charles Hathaway',
     author_email='chathaway@logrit.com',
-    packages=[''],
+    packages=['django_boostrap'],
     url='https://github.com/chuck211991/django_boostrap',
     license='BSD licence, see LICENCE.txt',
     description='Very Django app that containts the static Bootstrap files in static/{css,js}',
